35324f90b6bc9a456043328dc0483c714fa93c53
Evans Ibok <evansibok@gmail.com>
2020-11-07 08:52:27 +0100

    modify SECRET_KEY env variable

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'you-cant-tell-me-nothing'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-cant-tell-me-nothing'
